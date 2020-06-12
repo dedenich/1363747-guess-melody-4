794cd1659eb12e23842dfc73bce63105b70cf32a
Dedenich <sergbrst@tut.by>
2020-06-12 18:40:36 +0300

    Учебный проект: валидируй, как никогда не валидировал

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import WelcomeScreen from "./WelcomeScreen.jsx";
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {mistakesCount} = props;
+const App = () => {
   return <div>
-    <WelcomeScreen mistakesCount={mistakesCount}/>
+    <WelcomeScreen
+      mistakesCount={0}
+    />
   </div>;
 };
 
